1090099a6ebccae241abcadb196c27ee8e2129bf
Swayam Tejas Padhy <swayamtejasp@gmail.com>
2023-04-01 09:27:51 +0530

    Prettified the jquery js file

--- frontend/background.js
+++ frontend/background.js
@@ -1,6 +1,6 @@
 chrome.extension.onRequest.addListener(function(prediction){
     if (prediction == 1){
-        alert("Warning: Phishing detected!!");
+        alert("Warning: Phishing Suspected!!");
     }
     else if (prediction == -1){
         alert("No phishing detected");
